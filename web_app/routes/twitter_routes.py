e0cd7449804321dc56af6ab0d430b971f9cb049b
rubyclaguna <rubyclaguna@gmail.com>
2020-04-26 19:25:19 -0700

    storing user info in db

--- web_app/routes/twitter_routes.py
+++ web_app/routes/twitter_routes.py
@@ -1,5 +1,6 @@
 
 from flask import Blueprint, jsonify # render_template, request, flash, redirect
+from web_app.models import db, User, Tweet, parse_records
 
 #from web_app.models import db, Book, parse_records
 from web_app.services.twitter_service import api as twitter_api
@@ -12,6 +13,16 @@ def fetch_data(screen_name):
     
     # fetch user info
     user = twitter_api.get_user(screen_name)
+
+    # store user info in database
+    db_user = User.query.get(user.id) or User(id=user.id)
+    db_user.screen_name = user.screen_name
+    db_user.name = user.name
+    db_user.location = user.location
+    db_user.followers_count = user.followers_count
+
+    db.session.add(db_user)
+    db.session.commit()
 
     #fetch their tweets 
     statuses = twitter_api.user_timeline(screen_name, tweet_mode="extended", count=35, exclude_replies=True, include_rts=False)
