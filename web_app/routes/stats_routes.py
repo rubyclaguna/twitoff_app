7511bf26fb42363f1c3abb77aaa9a98a8d1f15ec
rubyclaguna <rubyclaguna@gmail.com>
2020-04-28 20:40:17 -0700

    prediction form

--- web_app/routes/stats_routes.py
+++ web_app/routes/stats_routes.py
@@ -15,3 +15,18 @@ def iris():
     result = str(clf.predict(X[:2, :]))
     print("PREDICTION", result)
     return result
+
+
+@stats_routes.route('/stats/predict', methods=["POST"]) 
+def twitoff_predict():
+    print("PREDICT ROUTE...")
+    print("FORM DATA:", dict(request.form))
+    #> {'screen_name_a': 'elonmusk', 'screen_name_b': 's2t2', 'tweet_text': 'Example tweet text here'}
+    screen_name_a = request.form["screen_name_a"]
+    screen_name_b = request.form["screen_name_b"]
+    tweet_text = request.form["tweet_text"]
+
+
+    print(screen_name_a, screen_name_b, tweet_text)
+
+    return "OK (TODO)"
